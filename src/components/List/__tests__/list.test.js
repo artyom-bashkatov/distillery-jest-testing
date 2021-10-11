4d9acee0fe002d72f1c78f05573dba0ce65f60c5
Artyom Bashkatov <artyom.bashkatov@distillery.com>
2021-10-11 15:35:39 +0300

    Extend test with snapshots and test cases with List-props and without props, check if render or null

--- src/components/List/__tests__/list.test.js
+++ src/components/List/__tests__/list.test.js
@@ -8,6 +8,24 @@ describe('List component', () => {
   test('List renders', () => {
     render(<List items={data} />);
 
+    expect(screen.getByRole('list')).toBeInTheDocument();
     expect(screen.getByText('html')).toBeInTheDocument();
   })
+
+  test('List renders without data', () => {
+    render(<List />);
+
+    // найти то, что может отсутствовать роли, соответствующей списку
+    expect(screen.queryByRole('list')).toBeNull();
+  })
+
+  test('List snapshot', () => {
+    const list = render(<List items={data} />);
+    expect(list).toMatchSnapshot();
+  });
+
+  test('List empty snapshot', () => {
+    const list = render(<List />);
+    expect(list).toMatchSnapshot();
+  });
 });
