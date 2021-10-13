53416f061a42d37d7ceb0d36e45da657263cc8ea
Artyom Bashkatov <artyom.bashkatov@distillery.com>
2021-10-13 14:07:28 +0300

    install cypress and write first e2e test

---
+++ cypress/integration/2-advanced-examples/first_test.spec.js
@@ -0,0 +1,8 @@
+/* eslint-disable no-undef */
+describe('Testing something on Google', () => {
+  it('I can search something', () => {
+    cy.visit('https://google.com');
+    cy.get(".gLFyf.gsfi").type('что-то').type('{enter}');
+    cy.contains('https://znanieinfo.ru');
+  });
+})
